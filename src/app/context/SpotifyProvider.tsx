a9803b46eb31001cd53acbddf72b8332022d8f78
Leandro <leandrocamposperes@gmail.com>
2023-11-13 22:09:59 -0300

    fix: more types and types

--- src/app/context/SpotifyProvider.tsx
+++ src/app/context/SpotifyProvider.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useContext, createContext, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { TrackObject } from "../api/lib/spotify";
@@ -14,19 +14,69 @@ export const usePlayer = () => {
 };
 
 interface PlayerState {
-  player: Spotify.SpotifyPlayer | null;
-  currentTrack: TrackObject | null;
-  currentDevice: string;
-  isPaused: boolean;
-  isActive: boolean;
+    player: Spotify.SpotifyPlayer | undefined;
+    currentTrack: WebPlaybackTrack | null;
+    currentDevice: string;
+    isPaused: boolean;
+    isActive: boolean;
 }
 
+interface Image {
+    url: string;
+}
+
+interface Album {
+    uri: string;
+    name: string;
+    images: Image[];
+}
+
+interface Artist {
+    uri: string;
+    name: string;
+}
+
+interface WebPlaybackTrack {
+    uri: string;
+    id: string | null;
+    type: string;
+    media_type: string;
+    name: string;
+    is_playable: boolean;
+    album: Album;
+    artists: Artist[];
+}
+
+interface WebPlaybackState {
+    context: {
+        uri: string | null;
+        metadata: Record<string, unknown> | null;
+    };
+    disallows: {
+        pausing: boolean;
+        peeking_next: boolean;
+        peeking_prev: boolean;
+        resuming: boolean;
+        seeking: boolean;
+        skipping_next: boolean;
+        skipping_prev: boolean;
+    };
+    paused: boolean;
+    position: number;
+    repeat_mode: number;
+    shuffle: boolean;
+    track_window: {
+        current_track: WebPlaybackTrack;
+        previous_tracks: WebPlaybackTrack[];
+        next_tracks: WebPlaybackTrack[];
+    };
+}
 
 export function usePlayerHook(): PlayerState {
     const { data: session } = useSession();
     const [player, setPlayer] = useState<Spotify.SpotifyPlayer>();
     const [is_loaded, setLoaded] = useState(false);
-    const [currentTrack, setTrack] = useState<TrackObject | null>(null);
+    const [currentTrack, setTrack] = useState<WebPlaybackTrack | null>(null);
     const [currentDevice, setDevice] = useState("");
     const [isPaused, setPaused] = useState(false);
     const [isActive, setActive] = useState(false);
@@ -43,7 +93,7 @@ export function usePlayerHook(): PlayerState {
         /* Initialize Player */
         window.onSpotifyWebPlaybackSDKReady = () => {
             const token = session?.accessToken;
-            const spotifyPlayer = new Spotify.Player({
+            const spotifyPlayer = new window.Spotify.Player({
                 name: "Web Playback SDK Quick Start Player",
                 getOAuthToken: (cb: any) => {
                     cb(token);
@@ -60,21 +110,24 @@ export function usePlayerHook(): PlayerState {
     }, [session?.accessToken, player, is_loaded]);
 
     if (player) {
-        player.addListener("ready", ({ device_id }) => {
+        player.addListener("ready", ({ device_id }: { device_id: string }) => {
             console.log("Ready with Device ID", device_id);
             setDevice(device_id);
         });
-        player.addListener("player_state_changed", (state) => {
-            if (!state) {
-                return;
-            }
-            setTrack(state.track_window.current_track);
-            setPaused(state.paused);
+        player.addListener(
+            "player_state_changed",
+            (state: WebPlaybackState) => {
+                if (!state) {
+                    return;
+                }
+                setTrack(state.track_window.current_track);
+                setPaused(state.paused);
 
-            player.getCurrentState().then((state) => {
-                !state ? setActive(false) : setActive(true);
-            });
-        });
+                player.getCurrentState().then((state: WebPlaybackState) => {
+                    !state ? setActive(false) : setActive(true);
+                });
+            }
+        );
         player.connect();
     }
 
@@ -83,11 +136,11 @@ export function usePlayerHook(): PlayerState {
         currentDevice,
         currentTrack,
         isPaused,
-        isActive
+        isActive,
     };
 }
 
-export function SpotifyProvider({ children}: { children: React.ReactNode }) {
+export function SpotifyProvider({ children }: { children: React.ReactNode }) {
     const player = usePlayerHook();
 
     return (
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,34 @@
 declare global {
   interface Window {
     onSpotifyWebPlaybackSDKReady?: () => void;
+    Spotify: {
+      Player: any; // Replace 'any' with the actual type if known
+    };
   }
-
+  namespace Spotify {
+    interface SpotifyPlayer {
+      _options: {
+        getOAuthToken: (callback: (token: string) => void) => void;
+        id: string;
+      };
+      connect: () => Promise<boolean>;
+      addListener: (
+        event: string,
+        cb: (state: Spotify.PlaybackState) => void
+      ) => void;
+      pause: () => Promise<void>;
+      resume: () => Promise<void>;
+      togglePlay: () => Promise<void>;
+      getCurrentState: () => Promise<Spotify.PlaybackState | null>;
+      setName: (name: string) => Promise<void>;
+      getVolume: () => Promise<number>;
+      setVolume: (volume: number) => Promise<void>;
+      previousTrack: () => Promise<void>;
+      nextTrack: () => Promise<void>;
+      seek: (positionMs: number) => Promise<void>;
+      getCurrentState: () => Promise<Spotify.PlaybackState | null>;
+    }
+  }
 }
 
 export {}
